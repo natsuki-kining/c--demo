205c2097e9b3044ae9044f38760ba2c17035466b
natsuki-kining <natsuki_kining@126.com>
2020-03-22 00:11:03 +0800

    string 类

---
+++ Test/string_test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	string str1("hello world");
+	string str2 = "hello c++";
+
+	cout << str1[0] << str1[1] << "," << str1 << endl;
+
+	cout << str2[0] << str2[1] << "," << str2 << endl;
+
+	cout << "please input something :";
+	
+	string str3;
+	cin >> str3;
+
+	cout << "input value is :" << str3 <<endl << "the string length is:" << str3.size() << endl;
+
+	//¸÷ÖÖapi
+}
